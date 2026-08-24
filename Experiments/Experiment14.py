0b3efad92794ed4b29050cc277140e0230a1efea
Hemanth Kumar S <venki009512@gmail.com>
2026-08-24 11:04:42 +0530

    Create Experiment14.py

---
+++ Experiments/Experiment14.py
@@ -0,0 +1,13 @@
+import nltk
+
+grammar = nltk.CFG.fromstring("""
+S -> NP VP
+NP -> 'John'
+VP -> 'runs'
+""")
+
+parser = nltk.ChartParser(grammar)
+
+for tree in parser.parse("John runs".split()):
+    print(tree)
+    tree.pretty_print()
